b952e462f99eb76f15fbdc8289ffaa32a8b24937
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-08-28 10:23:23 -0400

    plots amplitudes with option for single row

--- pipeplots.py
+++ pipeplots.py
@@ -39,9 +39,9 @@ palette_dict = {'ALMA-APEX':'k','JCMT-SMA':'k','SMT-LMT':'lime','ALMA-LMT':'medi
     'SMT-IRAM30':'dodgerblue'}
 
 palette_dict_rev = {k.split('-')[1]+'-'+k.split('-')[0]:v for k, v in palette_dict.items()}
-    palette_dict = {**palette_dict, **palette_dict_rev}
+palette_dict = {**palette_dict, **palette_dict_rev}
 
-def plot_amp_days(data,sour, bars_on=False, only_parallel=True,logscale=True):
+def plot_amp_days(data,sour, bars_on=False, only_parallel=True,logscale=True,palette_dict=palette_dict):
 
     if only_parallel==True:
         foo=data[(data.source==sour)&(data.polarization.str[0]==data.polarization.str[1])].copy()

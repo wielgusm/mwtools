82aa0b79ea0df21c1649f98ef4dd95ae8ef8e5cb
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-08-28 10:08:45 -0400

    plotting intrascan variability comparison

--- pipeplots.py
+++ pipeplots.py
@@ -6,6 +6,25 @@ from eat.hops import util as hu
 import matplotlib.pyplot as plt
 from eat.inspect import utils as ut
 
+dict_col_sour = {'1055+018': 'tan',
+     '1749+096': 'crimson',
+     '1921-293': 'mediumblue',
+     '3C273': 'lime',
+     '3C279': 'magenta',
+     '3C454.3': 'cyan',
+     '3C84': 'blueviolet',
+     'BLLAC': 'orange',
+     'CENA': 'darkgreen',
+     'CTA102': 'yellow',
+     'J0006-0623': 'tomato',
+     'J0132-1654': 'olivedrab',
+     'J1733-1304': 'salmon',
+     'J1924-2914': 'saddlebrown',
+     'M87': 'k',
+     'NGC1052': 'dodgerblue',
+     'OJ287': 'gold',
+     'SGRA': 'red'}
+
 def plot_amp_days(data,sour, bars_on=False, only_parallel=True,logscale=True):
 
     SMT2Z = {'ALMA': 'A', 'APEX': 'X', 'JCMT': 'J', 'LMT':'L', 'SMR':'R', 'SMA':'S', 'SMT':'Z', 'PV':'P','SPT':'Y'}
@@ -64,7 +83,7 @@ def plot_amp_days(data,sour, bars_on=False, only_parallel=True,logscale=True):
                 if logscale==True:
                     ax[couR,couC].set_yscale("log", nonposy='clip')
                 ax[couR,couC].set_ylim(ymin=0.1*np.min(foo.amp))
-                ax[couR,couC].set_ylim(ymax=1.2*np.max(foo.amp))
+                ax[couR,couC].set_ylim(ymax=1.1*np.max(foo.amp))
                 couP+=1
     plt.tight_layout()
     
@@ -75,3 +94,37 @@ def plot_amp_days(data,sour, bars_on=False, only_parallel=True,logscale=True):
     print("median snr {}".format(np.median(foo.snr)))
     print("=========================================")
     plt.show()
+
+
+def compare_uvf_apc(apc,uvf):
+
+    uvf,apc=ut.match_frames(uvf_sc.copy(),apc_sc.copy(),['source','band','polarization','scan_id','baseline'])
+    apc['var_before'] = uvf['std_by_mean']
+    apc['var_after'] = apc['std_by_mean']
+    data=apc.copy()
+    sns.set_style('whitegrid')
+    baseL = sorted(list(data.baseline.unique()))
+    ncol = int(np.ceil(len(baseL)/2))
+    num_base = (np.ceil((np.asarray(range(2*ncol))+0.1)/2))
+    diccol=dict(zip(baseL,num_base))
+    apc['basenum'] = list(map(lambda x: diccol[x],apc.baseline))
+
+    for basenum in sorted(apc.basenum.unique()):
+        data=apc[apc.basenum==basenum]
+        max_plot = np.maximum(np.max(data.var_before),np.max(data.var_after))
+        min_plot = np.minimum(np.min(data.var_before),np.min(data.var_after))
+        sg=sns.lmplot(data=data,x='var_before',y='var_after',hue='source',col='baseline',fit_reg=False,sharey=False,sharex=False,
+                     palette=dict_col_sour)
+        ax1 = sg.fig.axes[0]
+        hm1y=ax1.get_ylim()
+        hm1x=ax1.get_xlim()
+        ax1.plot([min_plot,max_plot],[min_plot,max_plot],'k--')
+        ax1.set_ylim(hm1y)
+        ax1.set_xlim(hm1x)
+        ax2 = sg.fig.axes[1]
+        hm2x=ax2.get_xlim()
+        hm2y=ax2.get_ylim()
+        ax2.plot([min_plot,max_plot],[min_plot,max_plot],'k--')
+        ax2.set_ylim(hm2y)
+        ax2.set_xlim(hm2x)
+        plt.show()

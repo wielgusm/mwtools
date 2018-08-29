44dfaa8be26387a105a44c96fd9efc2af3c5aa23
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-08-29 02:02:41 -0400

    plots for cphases

--- pipeplots.py
+++ pipeplots.py
@@ -227,7 +227,7 @@ def compare_coherence_time(coh0,incoh0,dict_col_sour=dict_col_sour,snr_cut=0):
     return coh[['datetime','source','expt_no','scan_id','polarization','band','baseline','amp_coh','amp_incoh','coh2incoh','snr_coh','snr_incoh']].copy()
 
 
-def bandpass_amplitude_consistency(data0,xmax=10):
+def bandpass_amplitude_consistency(data0,xmax=10,by_what='source'):
 
     data_lo, data_hi = ut.match_frames(data0[data0.band=='lo'].copy(),data0[data0.band=='hi'].copy(),['scan_id','baseline','polarization'])
     data = data_lo.copy()
@@ -248,15 +248,24 @@ def bandpass_amplitude_consistency(data0,xmax=10):
     plt.axvline(0,color='k')
     plt.xlabel('(LO-HI)/(thermal error)')
     plt.title('All data')
+
+    mad_abs=np.median(np.abs(data['amp_diff']))
+    mad_rel=np.median(np.abs(data['rel_diff']))
+    rangey = plt.ylim()
+    rangex = plt.xlim()
+    plt.text(0.5*rangex[1], 0.9*rangey[1], "MAD: %3.2f deg" % mad_abs , bbox=dict(facecolor='white', alpha=1.))
+    plt.text(0.5*rangex[1], 0.8*rangey[1], "REL MAD: %3.2f" % mad_rel , bbox=dict(facecolor='white', alpha=1.))
+
     plt.show()
 
     sourceL = sorted(list(data.source.unique()))
-    nplots=len(sourceL)
+    whatL = sorted(list(data[by_what].unique()))
+    nplots=len(whatL)
     ncols=2
     nrows=int(np.ceil(nplots/ncols))
     fig, ax = plt.subplots(nrows,ncols,sharey='all',sharex='all',figsize=(ncols*7,nrows*5))
 
-    for cou,sour in enumerate(sourceL):
+    for cou,what in enumerate(whatL):
         nbins = int(np.sqrt(np.shape(data[data.source==sour])[0]))
         bins = np.linspace(-xmax,xmax,nbins)
         nrowL = int(np.floor(cou/2))
@@ -267,6 +276,13 @@ def bandpass_amplitude_consistency(data0,xmax=10):
         ax[nrowL,ncolL].axvline(0,color='k')
         ax[nrowL,ncolL].set_xlabel('(LO-HI)/(thermal error)')
         ax[nrowL,ncolL].set_title(sour)
+        mad_abs=np.median(np.abs(data[data[by_what]==what]['amp_diff']))
+        mad_rel=np.median(np.abs(data[data[by_what]==what]['rel_diff']))
+        rangey = ax[nrowL,ncolL].get_ylim()
+        rangex = ax[nrowL,ncolL].get_xlim()
+        ax[nrowL,ncolL].text(0.5*rangex[1], 0.9*rangey[1], "MAD: %3.2f deg" % mad_abs , bbox=dict(facecolor='white', alpha=1.))
+        ax[nrowL,ncolL].text(0.5*rangex[1], 0.8*rangey[1], "REL MAD: %3.2f" % mad_rel , bbox=dict(facecolor='white', alpha=1.))
+
     plt.show()
     return data
 
@@ -296,8 +312,8 @@ def bandpass_cphase_consistency(data0,xmax=10,by_what='source'):
     mad_rel=np.median(np.abs(data['rel_diff']))
     rangey = plt.ylim()
     rangex = plt.xlim()
-    plt.text(0.6*rangex[1], 0.9*rangey[1], "MAD: %3.2f deg" % mad_abs , bbox=dict(facecolor='white', alpha=1.))
-    plt.text(0.6*rangex[1], 0.8*rangey[1], "REL MAD: %3.2f" % mad_rel , bbox=dict(facecolor='white', alpha=1.))
+    plt.text(0.5*rangex[1], 0.9*rangey[1], "MAD: %3.2f deg" % mad_abs , bbox=dict(facecolor='white', alpha=1.))
+    plt.text(0.5*rangex[1], 0.8*rangey[1], "REL MAD: %3.2f" % mad_rel , bbox=dict(facecolor='white', alpha=1.))
 
     plt.show()
 

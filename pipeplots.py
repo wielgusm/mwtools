2912c2ec6ee1591401d2278af1d49b7702af0fea
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-10-10 08:58:59 -0400

    estimate sys_noise

--- pipeplots.py
+++ pipeplots.py
@@ -918,10 +918,12 @@ def get_systematic(data,absolute,error):
     import scipy.optimize as so
     absolut = np.asarray(data[absolute])
     err = np.asarray(data[error])
-    print(np.median(np.abs(absolut/np.sqrt(err**2))/0.67449))
-    fun0 = lambda x: np.median( np.abs(absolut/np.sqrt(err**2 + x**2)) )/0.67449 -1.
-    print(fun0(0),fun0(10))
-    s0 = so.brentq(fun0, 0, 100)
+    m0 = np.median(np.abs(absolut/np.sqrt(err**2))/0.67449)
+    if m0>1.:
+        fun0 = lambda x: np.median( np.abs(absolut/np.sqrt(err**2 + x**2)) )/0.67449 -1.
+        #print(fun0(0),fun0(10))
+        s0 = so.brentq(fun0, 0, 100)
+    else: s0=0.
     return s0
 
 def trivial_cphase(data0,xmax=10,whichB='all',by_what='source',est_sys=False):
